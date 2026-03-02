4abf497ebb38d88e4589791303708c1da9eb19c1
Sapphire <sapphiresgfx@gmail.com>
2026-03-01 22:08:39 -0600

    disable strict mode to fix build

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("b7bd60280159458a")
+/******/ 		__webpack_require__.h = () => ("b409e63ed0f2e120")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "056703aa42e3cce9"; }
+/******/ 		__webpack_require__.h = function() { return "4e4cde920bf14f22"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
